c59ab664c74d10410b0eec819d4b397af86b9331
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-10 20:31:41 +0300

    🔧 fix(MealDetail.js): import Image component from react-native to display meal image ✨ feat(MealDetail.js): display meal details including image, title, duration, complexity, affordability, ingredients, and steps

--- screen/MealDetail.js
+++ screen/MealDetail.js
@@ -1,13 +1,51 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { MEALS } from '../data/dummy-data';
 
 function MealDetail({ route: { params } }) {
+  const { mealId } = params;
+  const meal = MEALS.find((meal) => meal.id === mealId);
+  const {
+    imageUrl,
+    title,
+    duration,
+    complexity,
+    affordability,
+    ingredients,
+    steps
+  } = meal;
+
   return (
     <View>
-      <Text>MealDetail {params.mealId}</Text>
+      <Image source={{ uri: imageUrl }} />
+      <Text>{title}</Text>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{duration}</Text>
+        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+      </View>
+      <Text>Ingredients</Text>
+      {ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12
+  }
+});
 
 export default MealDetail;
